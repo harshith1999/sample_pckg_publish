f56ce58c6dfe70973f1f3487101bc279e7561c35
harshith1999 <52190424+harshith1999@users.noreply.github.com>
2023-05-22 12:26:15 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+     name='sample_pckg_publish',  
+     version='0.1',
+     license='MIT',
+     author="harshith",
+     author_email='harshith@gmail.com',
+     packages=find_packages('src'),
+     package_dir={'': 'src'},
+     description="A new package",
+     url='https://github.com/harshith1999/sample_pckg_publish',
+     keywords='sample_pckg_publish',
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
